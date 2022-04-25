b36979e5c1b278cbe8f7fae0e58bec3f45874238
brazillucas <l.cardoso64@hotmail.com>
2022-04-25 02:04:36 -0300

    Tirando códigos desnecessários

--- projeto_02/script.js
+++ projeto_02/script.js
@@ -66,20 +66,7 @@ function timer () {
     
 }
 
-//intervalClock = setInterval(setDate, 1000);
-/* 
-function setDate(){
-    const currentDate = new Date();
-    const seconds = currentDate.getSeconds() / 60;
-    const minutes = (seconds + currentDate.getMinutes()) / 60;
-    const hours = (minutes + currentDate.getHours()) / 12;
-    setRotation(sec, seconds);
-    setRotation(min, minutes);
-    setRotation(hour, hours);
-} */
-
 function setRotation(element, rotationRatio) {
-    console.log("teste");
     element.style.setProperty('--rotation', rotationRatio * 360);
 
 }
